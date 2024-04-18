137e88f6f2d17628666047ac8cbe47c4e09c71dd
Lemony <Lemony>
2024-04-18 20:13:12 +0900

    Initial Commit

---
+++ kumkumm/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  kumkumm
+//
+//  Created by JIN LEE on 4/18/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+//            Image(systemName: "pencil.and.outline")
+//                .imageScale(.large)
+//                .foregroundStyle(.tint)
+//                .padding()
+//                .frame
+            Text("sistuzisama, kumkumm")
+                .font(.title3)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ kumkumm/kumkummApp.swift
@@ -0,0 +1,17 @@
+//
+//  kumkummApp.swift
+//  kumkumm
+//
+//  Created by JIN LEE on 4/18/24.
+//
+
+import SwiftUI
+
+@main
+struct kumkummApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
